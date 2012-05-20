4c499a64fc83b4fbe2978bea90ad3e12a4965311
Dumitru Sipos <dumitru.sipos@gmail.com>
2012-05-20 11:09:26 +0200

    initial version of GoogleAppEngine based web page: tutorial, hello world

---
+++ helloworld/helloworld.py
@@ -0,0 +1,34 @@
+import cgi
+import webapp2
+
+from google.appengine.api import users
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("""
+          <html>
+            <body>
+              <form action="/sign" method="post">
+                <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                <div><input type="submit" value="Sign Guestbook"></div>
+              </form>
+            </body>
+          </html>""")
+    def get_original_01(self):
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write('Hello, webapp World!! ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/sign', Guestbook)],
+                               debug=True)
