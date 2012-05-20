ee12ba679bfa8525e9966981936cae667778f331
Dumitru Sipos <dumitru.sipos@gmail.com>
2012-05-20 19:53:22 +0200

    Using the datastore...

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,33 +1,61 @@
 import cgi
+import datetime
+import urllib
 import webapp2
 
+from google.appengine.ext import db
 from google.appengine.api import users
+
+# a complete example using the datastore:
+
+class Greeting(db.Model):
+    """Models an individual Guestbook entry with an author, content and date."""
+    author = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
+
+def guestbook_key(guestbook_name=None):
+    """Contstruct a Datastore key for a Guestbook entity with guestbook_name."""
+    return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        self.response.out.write('<html><body>')
+        guestbook_name=self.request.get('guestbook_name')
+        greetings = db.GqlQuery("SELECT * "
+                            "FROM Greeting "
+                            "WHERE ANCESTOR IS :1 "
+                            "ORDER BY date DESC LIMIT 10",
+                            guestbook_key(guestbook_name))
+        for greeting in  greetings:
+            if greeting.author:
+                self.response.out.write(
+                    '<b>%s</b> wrote:' % greeting.author.nickname())
+            else:
+                self.response.out.write('An anonymous person wrote:')
+            self.response.out.write('<blockquote>%s</blockquote>' %
+                                    cgi.escape(greeting.content))
         self.response.out.write("""
-          <html>
-            <body>
-              <form action="/sign" method="post">
-                <div><textarea name="content" rows="3" cols="60"></textarea></div>
-                <div><input type="submit" value="Sign Guestbook"></div>
-              </form>
-            </body>
-          </html>""")
-    def get_original_01(self):
-        user = users.get_current_user()
-
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, webapp World!! ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+          <form action="/sign?%s" method="post">
+            <div><textarea name="content" rows="3" cols="60"></textarea></div>
+            <div><input type="submit" value="Sign Guestbook"></div>
+          </form>
+          <hr>
+          <form>Guestbook name: <input value="%s" name="guestbook_name">
+          <input type="submit" value="switch"></form>
+        </body>
+      </html>""" % (urllib.urlencode({'guestbook_name': guestbook_name}),
+                          cgi.escape(guestbook_name)))
 
 class Guestbook(webapp2.RequestHandler):
     def post(self):
-        self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.get('content')))
-        self.response.out.write('</pre></body></html>')
+        guestbook_name = self.request.get('guestbook_name')
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        if users.get_current_user():
+            greeting.author = users.get_current_user()
+        greeting.content = self.request.get('content')
+        greeting.put()
+        self.redirect('/?'+urllib.urlencode({'guestbook_name':guestbook_name}))
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/sign', Guestbook)],
